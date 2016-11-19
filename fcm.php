6782f86dbece26b3514d58852ba1de76bf82648c
ramarajan-tester <ramarajan.tester@gmail.com>
2016-11-19 13:16:14 +0530

    Update fcm.php

--- fcm.php
+++ fcm.php
@@ -10,7 +10,7 @@ Parameter Example
 //FCM api URL
 $url = 'https://fcm.googleapis.com/fcm/send';
 //api_key available in Firebase Console -> Project Settings -> CLOUD MESSAGING -> Server key
-$server_key = 'AIzaSyDGM0hPh6v3auyAwLJmKAO8NSqUf30uvLk';
+$server_key = 'AIzaSyBWNop1S48dmcnsLOJS4XQkpls3K4c9nfE';
 $fields = array();
 
 $notification["title"] ="Test";
